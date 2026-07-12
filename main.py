328fe07d05d25f083d01d11efeaa7ceb2110d21f
Harsh kumar Shukla <shuklaharsh202115@gmail.com>
2026-07-12 13:22:20 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+
+print('========== Smart File Organizer Pro ==========')
+folder_path= input('''Enter Folder Path:
+ 
+''')
+print()
+
+if os.path.isdir(folder_path):
+    print('✔ Folder Found')
+    print()
+    print(folder_path)
+    items = os.listdir(folder_path)
+    for item in items:
+        print(item)
+else:
+    print('❌ Invalid Folder Path')
+
+
