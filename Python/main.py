99f5e22e22d89ce86e0e64cb5bace17c84b1b737
Harsh kumar Shukla <shuklaharsh202115@gmail.com>
2026-07-16 15:32:43 +0530

    Completed Milestone 8 - Added duplicate file handling

--- Python/main.py
+++ Python/main.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 print('========== Smart File Organizer Pro ==========')
 folder_path= input('''Enter Folder Path:
@@ -7,12 +8,13 @@ folder_path= input('''Enter Folder Path:
 print()
 
 if os.path.isdir(folder_path):
-    print('✔ Folder Found')
+    print(' Folder Found')
     print()
     print('Files Found:')
     print()
 
     items = os.listdir(folder_path)
+    
     FILE_TYPES= { 
     
         ".jpg": "Images",
@@ -41,15 +43,29 @@ if os.path.isdir(folder_path):
             file_name, extension = os.path.splitext(item)
             category = FILE_TYPES.get(extension, 'Others')
             category_folder = os.path.join(folder_path, category)
-            os.makedirs(category_folder, exist_ok= True)
+            os.makedirs(category_folder, exist_ok = True)
             print(item, '----->', category)
+            destination_path= os.path.join(category_folder, item)
+            
+            if os.path.exists(destination_path):
+                counter = 1 
+                while True:
+                    rec_file = f"{file_name}({counter}){extension}"
+                    new_destination_path= os.path.join(category_folder, rec_file)
+                    if os.path.exists(new_destination_path):
+                        counter+=1
+                        
 
+                    else:
+                        shutil.move(full_path, new_destination_path)
+                        break
 
-
-    
-
+            else:
+                shutil.move(full_path, destination_path)
+                
+                
 else:
-    print('❌ Invalid Folder Path')
+    print(' Invalid Folder Path')
 
 
 
