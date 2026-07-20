9edbc967f0c95fae505e82e37b6166b523bc5b6d
Harsh kumar Shukla <shuklaharsh202115@gmail.com>
2026-07-20 12:30:39 +0530

    Complete Smart File Organizer Pro v1.0

--- Python/main.py
+++ Python/main.py
@@ -1,5 +1,10 @@
 import os
 import shutil
+import logging
+logging.basicConfig(
+    filename="organizer.log",
+    level=logging.INFO
+)
 
 print('========== Smart File Organizer Pro ==========')
 folder_path= input('''Enter Folder Path:
@@ -34,11 +39,17 @@ if os.path.isdir(folder_path):
 
 
     }
-    for item in items:
-        full_path= os.path.join(folder_path , item)
 
+    total_files = 0
+    success_files = 0
+    failed_files = 0
+    for item in items:
+
+        full_path= os.path.join(folder_path , item)
+          
 
         if os.path.isfile(full_path):
+            total_files += 1
             
             file_name, extension = os.path.splitext(item)
             category = FILE_TYPES.get(extension, 'Others')
@@ -48,85 +59,50 @@ if os.path.isdir(folder_path):
             destination_path= os.path.join(category_folder, item)
             
             if os.path.exists(destination_path):
-                counter = 1 
+                counter=1
                 while True:
-                    rec_file = f"{file_name}({counter}){extension}"
+
+                    rec_file = f"{file_name} ({counter}){extension}"
                     new_destination_path= os.path.join(category_folder, rec_file)
                     if os.path.exists(new_destination_path):
                         counter+=1
-                        
-
                     else:
-                        shutil.move(full_path, new_destination_path)
+                        final_destination = new_destination_path
                         break
+            
 
             else:
-                shutil.move(full_path, destination_path)
-                
-                
+                final_destination = destination_path
+
+            
+            file = os.path.basename(final_destination)
+            
+
+            try:
+                shutil.move(full_path, final_destination)
+                success_files +=1
+                print(f"Success: Moved '{full_path}' to '{final_destination}'")
+                logging.info(f"Success: Moved '{full_path}' to '{final_destination}'")
+            except FileNotFoundError:
+                failed_files +=1
+                print(f"Error: Source file ya path '{full_path}' nahi mila.")
+            except PermissionError:
+                print(f"Error: Permission denied! File move karne ka access nahi hai.")
+            except shutil.Error as e:
+                print(f"Shutil Error: {e}")
+            except Exception as e:
+                print(f"An unexpected error occurred: {e}")
+            
+    print("\n========== Summary ==========")
+    print(f"Total Files          : {total_files}")
+    print(f"Moved Successfully   : {success_files}")
+    print(f"Failed               : {failed_files}")
+    print("============================")
+
+            
+               
 else:
     print(' Invalid Folder Path')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
